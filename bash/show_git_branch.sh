44736356c45250bd62b95e7e04327aec775a471e
okeysea <ihgboss@gmail.com>
2021-06-09 23:38:26 +0900

    Update color, display

--- bash/show_git_branch.sh
+++ bash/show_git_branch.sh
@@ -1,18 +1,23 @@
 function parse_git_branch {
-  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
+  # git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [ᚠ \1]/'
 }
 
 function promps {
   local BLUE="\[\e[1;34m\]"
   local RED="\[\e[1;31m\]"
-  local GREEN="\[\e[1;32m]\]"
+  local GREEN="\[\e[1;32m\]"
   local WHITE="\[\e[00m\]"
   local GRAY="\[\e[1;37m\]"
 
+  local BRANCH="\[\e[1;35m\]"
+
   case $TERM in
-    xterm*) TITLEBAR='\[\e]0;\E\007\]';;
+    xterm*) TITLEBAR='\[\e]0;\W\007\]';;
     *)      TITLEBAR="";;
   esac
   local BASE="\u@\h"
-  PS1="${TITLEBAR}${GREEN}${BASE}${WHITE}:${BLUE}\W${GREEN}\$(parse_git_branch)${BLUE}\${WHITE} "
+  PS1="${TITLEBAR}${GREEN}${BASE}${WHITE}:${BLUE}\w${BRANCH}\$(parse_git_branch)${BLUE}\$${WHITE} "
 }
+
+promps
