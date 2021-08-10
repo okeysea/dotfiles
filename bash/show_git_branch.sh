39b7fead7e24324b4ae50dc8cb9ac5bbd6483102
okeysea <okeysea000@gmail.com>
2021-08-10 11:39:03 +0900

    Add support for ZSH

--- bash/show_git_branch.sh
+++ bash/show_git_branch.sh
@@ -3,7 +3,7 @@ function parse_git_branch {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [ᚠ \1]/'
 }
 
-function promps {
+function promps_bash {
   local BLUE="\[\e[1;34m\]"
   local RED="\[\e[1;31m\]"
   local GREEN="\[\e[1;32m\]"
@@ -20,4 +20,28 @@ function promps {
   PS1="${TITLEBAR}${GREEN}${BASE}${WHITE}:${BLUE}\w${BRANCH}\$(parse_git_branch)${BLUE}\$${WHITE} "
 }
 
-promps
+function promps_zsh {
+  local BLUE=$'%{\e[1;34m%}'
+  local RED=$'%{\e[1;31m%}'
+  local GREEN=$'%{\e[1;32m%}'
+  local WHITE=$'%{\e[00m%}'
+  local GRAY=$'%{\e[1;37m%}'
+
+  local BRANCH=$'%{\e[1;35m%}'
+
+  case $TERM in
+    xterm*) TITLEBAR=$'%{\e]0;\W\007%}';;
+    *)      TITLEBAR="";;
+  esac
+  local BASE="%n@%m"
+
+  DISABLE_AUTO_TITLE=true
+  setopt PROMPT_SUBST
+  PROMPT="${TITLEBAR}${GREEN}${BASE}${WHITE}:${BLUE}%c${BRANCH}\$(parse_git_branch)${BLUE}\$${WHITE} "
+}
+
+if [ -n "${BASH_VERSION-}" ]; then
+  promps_bash
+elif [ -n "${ZSH_VERSION-}" ]; then
+  promps_zsh
+fi
